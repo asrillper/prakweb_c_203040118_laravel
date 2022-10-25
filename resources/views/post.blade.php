8065fec46d6b2108e635679dd0ef0a127f1ad2c1
asrillper <203040118@mail.unpas.ac.id>
2022-10-25 17:54:21 +0700

    menyelesaikan video kedelapan laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,12 @@
 @section('container')
 <article>
     <h2 class="mb-5">{{ $post->title }}</h2>
+
+    <p>By. Asril Permana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
+
+
 </article>
 
 <a href="/blog">Back to Posts</a>
