a89b8b93e996ed41775661b4437fe95512d6338b
asrillper <203040118@mail.unpas.ac.id>
2022-10-31 22:18:08 +0700

    menyelesaikan video keduabelas laravel

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -20,8 +20,10 @@ class PostFactory extends Factory
             'title' => fake()->sentence(mt_rand(2, 8)),
             'slug' => fake()->slug(),
             'excerpt' => fake()->paragraph(),
-            'body' => fake()->paragraph(mt_rand(5, 10)),
-            'user_id' => mt_rand(1,3),
+            'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
+            ->map(fn($p) => "<p>$p</p>")
+            ->implode(''),
+            'user_id' => \mt_rand(1,3),
             'category_id' => mt_rand(1,2)
         ];
     }
