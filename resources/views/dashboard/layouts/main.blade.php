99e2e1c1d17255bdc8d1435c6f69b286aea5861e
asrillper <203040118@mail.unpas.ac.id>
2022-11-26 14:46:19 +0700

    menyelesaikan video keduapuluhdua laravel

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Asrillper Blog | Dashboard</title>
+    <title>{{ auth()->user()->name }} | Dashboard</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -43,7 +43,8 @@
 
   <div class="mb-3">
           <label for="image" class="form-label">Post Image</label>
-          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+          <img class="img-preview img-fluid mb-3 col-sm-5">
+          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
           @error('image')
           <p class="text-danger">{{ $message }}</p>
           @enderror
@@ -72,9 +73,22 @@
         .then(data => slug.value = data.slug)
     });
 
+    
+
     document.addEventListener('trix-file-accept', function(e) {
       e.preventDefault();
     });
+
+    function previewImage() {
+            const image =  document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+            imgPreview.style.display = 'block';
+            const ofReader = new FileReader();
+            ofReader.readAsDataURL(image.files[0]);
+            ofReader.onload = function(oFREvent) {
+                imgPreview.src = oFREvent.target.result;
+            }
+        }
 </script>
 
 @endsection
